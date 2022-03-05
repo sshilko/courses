f07305f9a75428d4f3a1daf16774f76ccf1ed082
Sergei Shilko <sergey.shilko@gmail.com>
2022-03-05 13:30:24 +0100

    finish

--- symfonycasts/symfony5track/06_symfony_apiplatform1/start/src/Entity/CheeseListing.php
+++ symfonycasts/symfony5track/06_symfony_apiplatform1/start/src/Entity/CheeseListing.php
@@ -2,25 +2,53 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\NumericFilter;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
  *     collectionOperations={"get", "post"},
  *     itemOperations={
- *     "get",
+ *     "get"={
+ *           "normalization_context"={"groups"={"cheeze_listing:read", "cheeze_listing:item:get"}}
+ *
+ *
+ *     },
  *     "put"
  *     },
- *     normalizationContext={"groups"={"cheeze_listing:read"}, "swagger_definition_name"="Read"},
- *     denormalizationContext={"groups"={"cheeze_listing:write"}, "swagger_definition_name"="Write"},
- *     shortName="cheeses"
+ *     normalizationContext={"groups"={"cheeze_listing:read"}},
+ *     denormalizationContext={"groups"={"cheeze_listing:write"}},
+ *     shortName="cheeses",
+ *     attributes={
+ *     "pagination_items_per_page"=2,
+ *     "formats"={"jsonld", "json", "html", "jsonhal", "csv"={"text/csv"}}
+
+ *     }
  * )
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
+ * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
+ * @ApiFilter(SearchFilter::class, properties={
+ *     "title": "partial",
+ *     "description": "partial",
+ *     "owner": "exact",
+ *     "owner.username": "partial"
+ *
+ *
+ *     })
+ * @ApiFilter(RangeFilter::class, properties={"price"})
+ * @ApiFilter(NumericFilter::class, properties={"id"})
+ * @ApiFilter(PropertyFilter::class)
  */
 class CheeseListing
 {
@@ -33,13 +61,20 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"cheeze_listing:read", "cheeze_listing:write"})
+     * @Groups({"cheeze_listing:read", "cheeze_listing:write", "user:read", "user:write"})
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     min="2",
+     *     max="50",
+     *     maxMessage="Describe your cheese in 50 chars or less"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
      * @Groups({"cheeze_listing:read"})
+     * @Assert\NotBlank()
      */
     private $description;
 
@@ -47,7 +82,8 @@ class CheeseListing
      * Price in cents
      *
      * @ORM\Column(type="integer")
-     * @Groups({"cheeze_listing:read", "cheeze_listing:write"})
+     * @Groups({"cheeze_listing:read", "cheeze_listing:write", "user:read", "user:write"})
+     * @Assert\NotBlank()
      */
     private $price;
 
@@ -60,6 +96,14 @@ class CheeseListing
      * @ORM\Column(type="boolean")
      */
     private $isPublished = false;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="cheeseListings")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"cheeze_listing:read", "cheeze_listing:write"})
+     * @Assert\Valid()
+     */
+    private $owner;
 
     public function __construct(string $title)
     {
@@ -88,6 +132,18 @@ class CheeseListing
     {
         return $this->description;
     }
+
+    /**
+     * @Groups({"cheeze_listing:read"})
+     */
+    public function getShortDescription(): ?string
+    {
+        if (strlen($this->description) < 40) {
+            return $this->description;
+        }
+        return substr($this->description, 0, 40) . '...';
+    }
+
 
     public function setDescription(string $description): self
     {
@@ -145,12 +201,24 @@ class CheeseListing
     /**
      * Just text please
      *
-     * @Groups({"cheeze_listing:write"})
+     * @Groups({"cheeze_listing:write", "user:write"})
      * @SerializedName("description")
      */
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        $this->owner = $owner;
 
         return $this;
     }
